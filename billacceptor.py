769bab9409eb3686b2cbb69f7a48fd1652309a38
GTT008 <yusufridhobelasetiyadi500213@mail.ugm.ac.id>
2025-02-22 12:22:35 +0700

    V1.9

--- billacceptor.py
+++ billacceptor.py
@@ -277,6 +277,6 @@ def trigger_transaction():
 
     return jsonify({"status": "success", "message": "Transaksi dimulai"})
 
-if __name__= "__main__" :
+if __name__ == "__main__":
     pi.callback(BILL_ACCEPTOR_PIN, pigpio.RISING_EDGE, count_pulse)
     app.run(host="0.0.0.0", port=5000, debug=True)
