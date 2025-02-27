3686cc99aa7c847502ee0658035a47543a704d7c
GTT008 <yusufridhobelasetiyadi500213@mail.ugm.ac.id>
2025-02-27 15:43:27 +0700

    logs

--- log.py
+++ log.py
@@ -4,7 +4,7 @@ Address = 10.0.0.2/24                  # IP klien dalam VPN
 DNS = 8.8.8.8                          # Bisa diganti
 
 [Peer]
-PublicKey = OLQqg19CkSNNKK17kM9HTrr1LJ7kp/ZzpmGFSF84zhs=    # Ganti dengan P>
+PublicKey = CkH/C/r9q1ioTUey61YdAyivDeiRxr/aI8lpdUHpHVY=    # Ganti dengan P>
 Endpoint = 202.169.224.6:51820           # Ganti dengan IP Publik/DNS Server
 AllowedIPs = 0.0.0.0/0                  # Untuk akses semua jaringan
 PersistentKeepalive = 25
