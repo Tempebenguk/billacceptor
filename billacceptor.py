9f983192d77c06c3e37cd2df6bd8c218e649a3b5
GTT008 <yusufridhobelasetiyadi500213@mail.ugm.ac.id>
2025-03-12 11:32:41 +0700

    coba debug

--- billacceptor.py
+++ billacceptor.py
@@ -281,7 +281,7 @@ def trigger_transaction():
     
     while True:
         if transaction_active:
-            time.sleep(1) 
+            time.sleep(5) 
             continue
 
         log_transaction("🔍 Mencari payment token terbaru...")
