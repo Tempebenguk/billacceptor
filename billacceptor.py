6509ffd13a892cff9ff4ffd6f42a0d06b198a573
GTT008 <yusufridhobelasetiyadi500213@mail.ugm.ac.id>
2025-03-12 15:49:30 +0700

    menghilangkan reset timeout

--- billacceptor.py
+++ billacceptor.py
@@ -298,14 +298,6 @@ def get_bill_acceptor_status():
         "status": "success",
         "message": "Bill acceptor siap digunakan"
     }), 200 
-import time
-import datetime
-import requests
-
-import time
-import datetime
-import requests
-
 def trigger_transaction():
     global transaction_active, total_inserted, id_trx, payment_token, product_price, last_pulse_received_time, pending_pulse_count
 
